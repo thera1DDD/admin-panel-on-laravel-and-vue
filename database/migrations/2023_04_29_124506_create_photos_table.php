4e1c8d7097111aab3bac0a16839bc2c471bd601a
thera1DDD <tagir566666@gmail.com>
2023-04-29 17:23:46 +0300

    Проверка игнор env

---
+++ database/migrations/2023_04_29_124506_create_photos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('photoable_id')->nullable();
+            $table->string('photoable_type')->nullable();
+            $table->string('filename');
+            $table->integer('status')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
