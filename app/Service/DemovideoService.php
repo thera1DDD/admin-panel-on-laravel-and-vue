6959557ce67f9f7b46b340914a2377d2f47c2979
thera1DDD <tagir566666@gmail.com>
2023-06-07 20:07:09 +0300

    demovideo fixed x2

--- app/Service/DemovideoService.php
+++ app/Service/DemovideoService.php
@@ -10,21 +10,38 @@ class DemovideoService
 {
     public function store($data){
 
-//        $video = new Demovideo();
-//        $video->name = $data['name'];
-//        $video->description = $data['description'];
-//        $video->courses_id = $data['courses_id'];
-//        $video_file = $data['video_file'];
-//        $video_file_name = time() . '_' . $video_file->getClientOriginalName();
-//        $video_file_path = $video_file->storeAs('demovideo', $video_file_name, 'public');
-//        $video->video_file = $video_file_path;
-//        $video->save();
-        Demovideo::firstOrCreate($data);
+        if ($request->hasFile('main_image')) {
+            $file = $request->file('main_image');
+            $path = $file->store('images/courses', 'public');
+            $data['main_image'] = $path;
+        }
+        $data['main_image'] = Storage::disk('public')->url($data['main_image']);
+        $video = new Demovideo();
+        $video->poster = $data['poster'];
+        $video->name = $data['name'];
+        $video->description = $data['description'];
+        $video->courses_id = $data['courses_id'];
+        $video_file = $data['video_file'];
+        $video_file_name = time() . '_' . $video_file->getClientOriginalName();
+        $video_file_path = $video_file->storeAs('demovideo', $video_file_name, 'public');
+        $video->video_file = $video_file_path;
+        $data['video_file'] = Storage::disk('public')->url($data['video_file']);
+        $video->save();
+
 
     }
 
     public function update(Demovideo $demovideo,$data)
     {
-        $demovideo->update($data);
+        $video = new Demovideo();
+        $video->name = $data['name'];
+        $video->description = $data['description'];
+        $video->courses_id = $data['courses_id'];
+        $video_file = $data['video_file'];
+        $video_file_name = time() . '_' . $video_file->getClientOriginalName();
+        $video_file_path = $video_file->storeAs('demovideo', $video_file_name, 'public');
+        $video->video_file = $video_file_path;
+        $data['video_file'] = Storage::disk('public')->url($data['video_file']);
+        $video->save();
     }
 }
