95a84bc49458b9e5b8389ec6c8db71b70e4e4ead
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-06-25 16:23:01 +0300

    video play test x2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,7 +11,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 
 
     <!-- Fonts -->
--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.admin')
 
 @section('title')
-    Воспроиздведение
+    Воспроизведение
 @endsection
+
 @section('content')
     <div class="card">
         <div class="card-header">
@@ -12,26 +13,42 @@
             </div>
         </div>
         <!-- /.card-header -->
+        <!-- Ваш код HTML для воспроизведения видео -->
         <div class="card-body table-responsive p-0">
             <video id="video" controls></video>
-            <script>
-                var video = document.getElementById('video');
-                if(Hls.isSupported()) {
-                    var hls = new Hls();
-                    hls.loadSource("{{getImage($video_file_path)  }}");
-                    hls.attachMedia(video);
-                    hls.on(Hls.Events.MANIFEST_PARSED,function() {
-                        video.play();
-                    });
-                }
-                else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                    video.src = "{{ getImage($video_file_path) }}";
-                    video.addEventListener('loadedmetadata',function() {
-                        video.play();
-                    });
-                }
-            </script>
         </div>
-        <!-- /.card-body -->
+
     </div>
 @endsection
+
+<!-- Подключение библиотеки Hls.js -->
+<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Ваш собственный JavaScript код -->
+<script>
+    $(document).ready(function() {
+        // Получение ссылки на видеоэлемент
+        var video = document.getElementById('video');
+
+        if (Hls.isSupported()) {
+            // Создание экземпляра Hls
+            var hls = new Hls();
+
+            // Загрузка и воспроизведение M3U8 файла
+            hls.loadSource("{{ getImage($video_file_path) }}");
+            hls.attachMedia(video);
+
+            hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                video.play();
+            });
+        } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+            // Воспроизведение M3U8 файла с использованием стандартного видеоэлемента
+            video.src = "{{ getImage($video_file_path) }}";
+            video.addEventListener('loadedmetadata', function() {
+                video.play();
+            });
+        }
+    });
+</script>
+<!-- /.card-body -->
