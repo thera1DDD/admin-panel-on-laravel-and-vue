da33eb5c00689a0f9c543e9a58e72b01155b76d6
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-06-30 19:07:00 +0300

    auth name fixed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -62,10 +62,10 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{asset(getImage(auth()->user()->photo))}}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{asset(getImage(auth()->user()->photo) ?? route('login'))}}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">{{ auth()->user()->name}}</a>
+                        <a href="#" class="d-block">{{ auth()->user()->name?? route('login')}}</a>
                     </div>
                 </div>
 
