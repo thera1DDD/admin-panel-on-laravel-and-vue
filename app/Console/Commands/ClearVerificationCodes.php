18f12ca8bd7ca6bbb731f3a5ffc46b4a4a467468
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-07-11 01:58:54 +0300

    начал делать планировщик задач

---
+++ app/Console/Commands/ClearVerificationCodes.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class ClearVerificationCodes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'verification:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Удаляет верификационный код старше одной недели';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $weekAgo = now()->subSeconds(40);
+
+        User::whereNotNull('verification_code')
+            ->where('created_at', '<=', $weekAgo)
+            ->forceDelete();
+
+        $this->info('Verification codes cleared successfully.');
+    }
+}
