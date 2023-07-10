c5db0ab4804fee3f1085c1fd372c809da9c1a137
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-07-10 20:44:07 +0300

    добавил запрос для всех тестов x2

--- app/Http/Resources/Test/CourseResource.php
+++ app/Http/Resources/Test/CourseResource.php
@@ -20,7 +20,7 @@ class CourseResource extends JsonResource
             'name'=>$this->name,
             'main_image'=>$this->main_image,
             'sort'=>$this->sort,
-            'module' => ModuleResource::collection($this->whenLoaded('module')),
+            'modules' => ModuleResource::collection($this->whenLoaded('module')),
         ];
     }
 }
