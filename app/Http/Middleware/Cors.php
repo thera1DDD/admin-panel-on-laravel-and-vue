6242c38b85ae8ee43c994aa98aa56adaed038e5c
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-07-12 20:26:22 +0300

    cors fix 35

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,7 +9,6 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-
         $response->header('Access-Control-Allow-Origin', 'https://djigit-language.ru');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         return $response;
