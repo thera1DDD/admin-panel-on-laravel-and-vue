8fa6db248ae064079eae7c9ad276852086ec4248
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-07-12 21:02:29 +0300

    cors stuffasdf;plp

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,7 +10,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', "*")
-            ->header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS")
-            ->header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type");
+            ->header('Access-Control-Allow-Methods', "*")
+            ->header('Access-Control-Allow-Headers', "*");
     }
 }
