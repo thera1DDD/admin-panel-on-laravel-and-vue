6f6988ecb95831d86b532be22da3c24e4e2843fb
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-07-20 21:59:41 +0300

    api about us modifed

--- app/Http/Controllers/API/V1/AboutUsController.php
+++ app/Http/Controllers/API/V1/AboutUsController.php
@@ -13,7 +13,7 @@ class AboutUsController extends MainApiController
         $quotes = AboutUs::select('id', 'firstQuote', 'secondQuote', 'image','big_image','text_content')->get();
 //        $other = AboutUs::select('big_image','text_content')->whereNotNull('big_image')->get();
         if(!$quotes){
-            return $this->error('there is not quotes',404);
+            return $this->error('there is no quotes',404);
         }
         return response()->json(['data'=>$quotes]);
 
