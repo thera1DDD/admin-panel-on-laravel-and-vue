fe646d8bd5f793c7b2d97648e1c590c5e93c18ba
thera1DDD <79668700+thera1DDD@users.noreply.github.com>
2023-08-05 02:29:36 +0300

    закешировал обратный поиск

--- app/Http/Controllers/API/MainApiController.php
+++ app/Http/Controllers/API/MainApiController.php
@@ -64,11 +64,18 @@ class MainApiController extends Controller
             return response()->json(['data' => $result]);
         }
     }
-    public function searchBackward($word,$languages_id){
-        $translate = Translate::query();
+    public function searchBackward($word, $languages_id)
+    {
+        $word = trim(htmlspecialchars($word));
+        $cacheKey = "search_backward_{$word}_{$languages_id}";
         if ($word) {
-            $data =  $translate->with('word')->where('translate', 'like', "%{$word}%")->where('languages_id',$languages_id)->get();
-            return  BackwardsTranslateResource::collection($data);
+            $data = Cache::remember($cacheKey, now()->addMinutes(5), function () use ($word, $languages_id) {
+                return Translate::with('word')
+                    ->where('translate', 'like', "%{$word}%")
+                    ->where('languages_id', $languages_id)
+                    ->get(['id', 'translate', 'words_id']);
+            });
+            return BackwardsTranslateResource::collection($data);
         }
     }
 
